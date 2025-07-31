6839b0460123cf0c2bf6d19f7b005a8d1d15d8a5
Sashi-CSM <168248403+Sashi-CSM@users.noreply.github.com>
2025-07-31 18:31:37 +0530

    1234

---
+++ Test Suites/TS_Orange HR - Visual Testing.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS_Orange HR - Visual Testing</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>288e40bf-8929-49c1-b1af-b46a7541bf19</testSuiteGuid>
+   <testCaseLink>
+      <guid>eca9bf37-a6f6-48b4-a9bd-5fb102f7b453</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Orange HR/TC_Orange HR - Visual Testing 1</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
